731e5ab86ef70e73ecc7ac1f6c98f708f6da1b94
Janaeiw <40946510+Janaeiw@users.noreply.github.com>
2026-02-27 08:00:46 +0000

    Deploying to gh-pages from @ Janaeiw/vue3-vite2-h5-template-y@43831983e0859b4d91330f9c679e6a2fce7cdc24 🚀

--- static/js/authController-c776de51.js
+++
@@ -1,1 +0,0 @@
-import{P as r}from"./index-9715d4c9.js";const t=()=>r({url:"/user/info",method:"get",loading:!0});function n(e){return r({url:"/login",method:"POST",data:e,loading:!0})}export{t as f,n as l};
